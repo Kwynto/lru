e9b7c55f5dc7b07039e7b0f472030795549b66ab
Constantine Zavezeon <31433211+Kwynto@users.noreply.github.com>
2022-08-25 23:01:09 +0300

    Load, Store and helpers

--- lru.go
+++ lru.go
@@ -7,69 +7,121 @@ import (
 	"time"
 )
 
-// dataCache
-
+// The internal structure of the cache store.
 type dataCache struct {
+	queue  int64
 	result any
-	queue  time.Time
 }
 
-// Cache
-
+// Contract for the use of the cache.
 type Cache interface {
-	Get(key any) (any, error)
-	Set(key any, value any) bool
-	СheckUp(key any) bool
+	Load(key any) (any, error)
+	Store(key any, value any) bool
+	Balancing()
 }
 
+// Cache infrastructure.
 type cache struct {
-	data   sync.Map
-	volume int
+	data     map[string]dataCache
+	capacity int
+	latch    sync.Mutex
 }
 
+// Cache constructor.
 func New(size int) Cache {
 	return &cache{
-		data:   sync.Map{},
-		volume: size,
+		data:     make(map[string]dataCache, 0),
+		capacity: size,
+		latch:    sync.Mutex{},
 	}
 }
 
-func (c *cache) Get(key any) (any, error) {
-	keyStr, ok := MarshalKey(key)
+// Get an entry from the cache.
+func (c *cache) Load(key any) (any, error) {
+	keyStr, ok := marshalKey(key)
 	if !ok {
 		return nil, errors.New("can't get result from cache")
 	}
 
-	value, ok := c.data.Load(keyStr)
+	newTime := time.Now().Unix()
+
+	c.latch.Lock()
+	defer c.latch.Unlock()
+
+	value, ok := c.data[keyStr]
 	if !ok {
 		return nil, errors.New("can't get result from cache")
 	}
 
-	return value, nil
+	value.queue = newTime
+	c.data[keyStr] = value
+
+	return value.result, nil
 }
 
-func (c *cache) Set(key any, value any) bool {
-	keyStr, ok := MarshalKey(key)
+// Write a new value to the cache or update an old value.
+func (c *cache) Store(key any, value any) bool {
+	var valueStore dataCache
+
+	keyStr, ok := marshalKey(key)
 	if !ok {
 		return false
 	}
-	c.data.Store(keyStr, value)
+
+	c.сleanUp()
+
+	valueStore.queue = time.Now().Unix()
+	valueStore.result = value
+
+	c.latch.Lock()
+	c.data[keyStr] = valueStore
+	c.latch.Unlock()
+
 	return true
 }
 
-func (c *cache) СheckUp(key any) bool {
-	keyStr, ok := MarshalKey(key)
-	if !ok {
-		return false
-	}
-
-	_, ok = c.data.Load(keyStr)
-	return ok
+// Cache balancing on overflow reducing it by 10%
+func (c *cache) Balancing() {
+	// TODO: Балансировка кеша при переполнении уменьшая его на 10%
 }
 
-// Helper functions
+// Helper functions and metods
 
-func MarshalKey(in any) (string, bool) {
+// Retrieves the key of the oldest cache item
+func (c *cache) extractMinValue() string {
+	markerT := time.Now().Unix()
+	markerK := ""
+
+	// c.latch.Lock()
+	for key, value := range c.data {
+		if value.queue < markerT {
+			markerT = value.queue
+			markerK = key
+		}
+	}
+	// c.latch.Unlock()
+
+	return markerK
+}
+
+// Removing a cache element by key.
+func (c *cache) remove(key string) {
+	c.latch.Lock()
+	delete(c.data, key)
+	c.latch.Unlock()
+}
+
+// Clearing the cache of old items
+func (c *cache) сleanUp() {
+	if len(c.data) >= c.capacity {
+		minKey := c.extractMinValue()
+		c.remove(minKey)
+		c.сleanUp()
+	}
+}
+
+// Serialize any type to JSON to use as a key.
+func marshalKey(in any) (string, bool) {
 	out, err := json.Marshal(in)
 	if err != nil {
 		return "", false
